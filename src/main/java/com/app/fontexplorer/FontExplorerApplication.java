8081e68e6bad972c811e3ace2eb769cdb13c56a3
MarcoLaureano <mlaurean19@jaumebalmes.net>
2023-04-13 16:31:41 +0200

    proto_0

---
+++ src/main/java/com/app/fontexplorer/FontExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.app.fontexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FontExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FontExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/fontexplorer/FontExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fontexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
