e7bc9ff2439dbe664a398412e5ad43dbe7f95430
MarcoLaureano <mlaurean19@jaumebalmes.net>
2023-04-21 16:29:14 +0200

    proto_0.3

--- src/main/java/com/app/fontexplorer/Controller/UsuarioController.java
+++ src/main/java/com/app/fontexplorer/Controller/UsuarioController.java
@@ -3,14 +3,24 @@ package com.app.fontexplorer.Controller;
 
 import com.app.fontexplorer.Entities.Usuario;
 import com.app.fontexplorer.Repositories.UsuarioRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/seeUsuarios")
+    @ResponseBody
+    public List<Usuario> obtenerUsuarios() {
+        return usuarioRepository.findAll();
+    }
 }
--- src/main/java/com/app/fontexplorer/Repositories/UsuarioRepository.java
+++ src/main/java/com/app/fontexplorer/Repositories/UsuarioRepository.java
@@ -3,9 +3,10 @@ package com.app.fontexplorer.Repositories;
 import com.app.fontexplorer.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByUsuario(String usuario);
 
-    boolean existsByEmail(String email);
 }
